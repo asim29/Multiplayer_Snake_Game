3e630b5627e167c78ef54734b1fd0b1f8a17e74b
asim29 <asim.waheed29@gmail.com>
2019-03-11 20:47:32 +0500

    Add responsiveness to Start button

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,19 +4,17 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
-import CannonBall from './CannonBall';
-import CurrentScore from './CurrentScore';
+import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
-import Heart from './Heart';
 import StartGame from './StartGame';
 import Title from './Title';
 
 const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 
-  							100 - gameHeight, 
-  							window.innerWidth, 
-  							gameHeight];
+            100 - gameHeight, 
+            window.innerWidth, 
+            gameHeight];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -29,23 +27,38 @@ const Canvas = (props) => {
           <feDropShadow dx="1" dy="1" stdDeviation="2" />
         </filter>
       </defs>
-    	<Sky />
-    	<Ground />
-    	<CannonPipe rotation={props.angle} />
-    	<CannonBase />
-      <CannonBall position={{x:0, y:-100}} />
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
       <CurrentScore score={15} />
-      <FlyingObject position={{x: -150, y:-300}} />
-      <FlyingObject position={{x: 150, y:-300}} />
-      <Heart position={{x: -300, y: 35}} />
-      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-      <Title />
-   </svg>
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
+
+      { props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
+    </svg>
   );
 };
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
   trackMouse: PropTypes.func.isRequired,
-}
+  startGame: PropTypes.func.isRequired,
+};
+
 export default Canvas;
