93c2ee3c3f5d5580db02e5d9cfbe4fe98966ee7b
asim29 <asim.waheed29@gmail.com>
2019-03-11 19:13:04 +0500

    Add CannonBall

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 
@@ -21,6 +22,7 @@ const Canvas = (props) => {
     	<Ground />
     	<CannonPipe rotation={props.angle} />
     	<CannonBase />
+      <CannonBall position={{x:0, y:-100}} />
     </svg>
   );
 };
