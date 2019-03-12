02ce664bccc6ee56466596abe5473bfaf43a28f9
asim29 <asim.waheed29@gmail.com>
2019-03-12 11:54:04 +0500

    Add Auth0 user authorization

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'asim29.auth0.com',
+  clientID: 'ZgFWH7EKUS3pEffl6hwpubOjCI6FnKQZ',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
 	
   componentDidMount() {
+
     const self = this;
+    
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    })
+
     setInterval(() => {
         self.props.moveObjects(self.canvasMousePosition);
     }, 10);
+    
     window.onresize = () => {
       const cnv = document.getElementById('aliens-go-home-canvas');
       cnv.style.width = `${window.innerWidth}px`;
