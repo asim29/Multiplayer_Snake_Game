043a6a04c007b222c1180a188f63e16a423a3120
asim29 <asim.waheed29@gmail.com>
2019-03-12 13:00:24 +0500

    Link Client to Server using Socket.io

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -18,16 +18,6 @@ const Canvas = (props) => {
             window.innerWidth, 
             gameHeight];
 
-  const leaderboard = [
-    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/profile_image', },
-    { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/profile_image', },
-    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/profile_image', },
-    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/profile_image', },
-    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/profile_image', },
-    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/profile_image', },
-    { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/profile_image', },
-    { id: 'h8', maxScore: 146, name: 'Sebastián Peyrott', picture: 'https://twitter.com/speyrott/profile_image', },
-  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -50,9 +40,9 @@ const Canvas = (props) => {
         <g>
           <StartGame onClick={() => props.startGame()} />
           <Title />
-          <Leaderboard currentPlayer={leaderboard[6]} 
+          <Leaderboard currentPlayer={props.currentPlayer} 
                         authenticate={signIn} 
-                        leaderboard={leaderboard} />
+                        />
         </g>
       }
 
@@ -82,6 +72,24 @@ Canvas.propTypes = {
   }).isRequired,
   trackMouse: PropTypes.func.isRequired,
   startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })),
 };
+
+Canvas.defaultProps = {
+  currentPlayer: null,
+  players: null,
+};
+
 
 export default Canvas;
